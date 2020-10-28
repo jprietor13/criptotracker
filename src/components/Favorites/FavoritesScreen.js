71a92ebc9db370ada9ab0eeda39871d1401c8d2d
Juan Prieto <gadaf.78@hotmail.com>
2020-10-28 16:27:01 -0500

    feat: lista de favoritos

--- src/components/Favorites/FavoritesScreen.js
+++ src/components/Favorites/FavoritesScreen.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import FavoritesEmptyState from './FavoritesEmptyState';
+import CoinsItem from '../coins/CoinsItem';
 import Colors from '../../resources/Colors';
+import Storage from '../../libs/storage';
 
 class FavoritesScreen extends React.Component {
+
+    state = {
+        favorites: []
+    }
+
+    getFavorites = async () => {
+        try {
+            const allKeys = await Storage.instance.getAllkeys();
+            const keys = allKeys.filter((key) => key.includes("favorite-"));
+            const favs = await Storage.instance.multiGet(keys);
+            const favorites = favs.map((fav) => JSON.parse(fav[1]));
+            console.log("favs", favorites);
+            this.setState({ favorites });
+        } catch (err) {
+            console.log("get favorites err", err);
+        }
+    }
+
+    handlePress = (coin) => {
+        this.props.navigation.navigate("CoinDetail", { coin });
+    }
+
+    componentDidMount = () => {
+        this.getFavorites();
+        this.props.navigation.addListener("focus", this.getFavorites);
+    }
+
+    componentWillMount = () => {
+        this.props.navigation.removeListener("focus", this.getFavorites);
+    }
+
     render(){
+
+        const { favorites } = this.state;
+
         return(
             <View style={styles.container}>
-                <FavoritesEmptyState />
+                { favorites.length == 0 ? <FavoritesEmptyState /> : null } 
+                { favorites.length > 0 ?
+                    <FlatList 
+                        data={favorites}
+                        renderItem={({ item }) => <CoinsItem item={item} onPress={() => this.handlePress(item)}/> 
+                    }   
+                     /> : null
+                }
             </View>
         );
     }
